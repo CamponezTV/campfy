2d458e1e8314cbaede395658aa40eafb40602d17
CamponezTV <arthurcamponez2020@gmail.com>
2024-09-05 20:08:19 -0300

    First commit in months

---
+++ lib/uploadthing.ts
@@ -0,0 +1,6 @@
+import type { OurFileRouter } from '@/app/api/uploadthing/core';
+import { generateUploadButton } from "@uploadthing/react";
+import { generateUploadDropzone } from "@uploadthing/react";
+ 
+export const UploadDropzone = generateUploadDropzone<OurFileRouter>();
+export const UploadButton = generateUploadButton<OurFileRouter>();
